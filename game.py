b0919c3b21a79720da8388cf7ddd0d2c84cb9f30
Maga <mcmagakrytoy@gmail.com>
2025-04-13 21:41:06 +0300

    запуск программы,отображение дисплея

--- game.py
+++ game.py
@@ -1,4 +1,30 @@
-import road
+import pygame,road
+
+
+display = pygame.display.set_mode((700,400))
+name = pygame.display.set_caption("Dino")
+
 
 class Game:
-    ...
+    def __init__(self):
+        ...
+    
+
+    def road(self):
+        self.road = road.Road()
+
+
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                quit()
+
+        display.fill((255,255,255))
+        pygame.display.update()
+            
+if __name__ == "__main__":
+    main()
