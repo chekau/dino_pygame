ef8c2249aa670f8e923fd1e51f458a8bbcc70807
Maga <mcmagakrytoy@gmail.com>
2025-04-14 22:34:52 +0300

    создание новых методов класа game,дискретезация этого класса

--- main.py
+++ main.py
@@ -10,23 +10,42 @@ class Game:
     pygame.init()
 
     def __init__(self):
+        self.running = True
         self.display = pygame.display.set_mode((700,400))
         self.name = pygame.display.set_caption("Dino")
         self.color_display = (255,255,255)
         self.road = Road()
 
 
-    def game_loop(self):
-        running = True
-        while running:
-            for event in pygame.event.get():
+    def process_input(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
-                    pygame.quit()
-                    quit()
+                    self.running = False
+                    break
 
-            self.display.fill(self.color_display)
-            pygame.display.update()
+    def update_game(self):
+         ...
+
+    def render(self):
+        self.display.fill(self.color_display)
+        self.display.blit(self.road.image,(-100,300))
+        pygame.display.update()
+         
+
+
+    def game_loop(self):
+        while self.running:
+            self.process_input()
+            self.update_game()
+            self.render()
+            
+        pygame.quit()
+
+              
+        
+        
+           
+
             
     
     
--- road.py
+++ road.py
@@ -1,8 +1,12 @@
 import pygame
+import sys
 
 class Road:
     def __init__(self):
-        ...
+        self.road_image = pygame.image.load(r"static\road.png")
+        self.image = pygame.transform.scale(self.road_image,(1000,150))
+        
+
     
     
 
