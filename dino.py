ae51d577f8180cab1ba9570a36dde9458eaee99b
Maga <mcmagakrytoy@gmail.com>
2025-04-27 19:15:50 +0300

    разработка прыжка динозаврика

--- dino.py
+++ dino.py
@@ -7,9 +7,27 @@ class Dino:
     def __init__(self):
         self.dino_image = pygame.image.load(r"static/dino.png")
         self.image = pygame.transform.scale(self.dino_image,(70,70))
+        self.img_rect = self.image.get_rect()
+        self.img_rect.x = 50
+        self.img_rect.y = 250
+        self.vert_vel = 0
+        self.gravity = 1
+        self.is_jump = False
+        self.jump_max = 15
+        self.jump_count = 0
 
-    def jump():
-        ...
+    def jump(self):
+        self.is_jump = True
+        self.jump_count = self.jump_max
+
+        if self.is_jump:
+            self.img_rect.y -= self.jump_count
+            if self.jump_count > -self.jump_max:
+                self.jump_count -= 1
+            else:
+                self.is_jump = False 
+        
+
     def crash():
         ...
     def position():
--- main.py
+++ main.py
@@ -26,17 +26,22 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
                     break
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.road.dino.jump()
 
     def update_game(self):
         self.road.obstancle.move()
         self.road.add_obstancle()
+        self.road.dino.jump()
+        
 
     def render(self):
         self.display.fill(self.color_display)
         self.display.blit(self.road.image,(-150,120))
         self.display.blit(self.road.obstancle.image,(self.road.obstancle.rect.x,
                                                      self.road.obstancle.rect.y))
-        self.display.blit(self.road.dino.image,(50,250))
+        self.display.blit(self.road.dino.img_rect,(50,250))
         
         
         pygame.display.flip()
